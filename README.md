e1f10a7b9a3bb8aab95c78f585693af5d550efdd
wanted2 <77376569+wanted2@users.noreply.github.com>
2021-05-31 00:17:55 +0700

    added readme

---
+++ README.md
@@ -0,0 +1,8 @@
+An example of using MLKit Text Recognition with CameraX
+=====
+
+This is the code for my [blog post](https://www.caineng.in/).
+Use with Android Studio.
+The usage is as follows:
+
+![](./screen.gif)
--- mocr/src/androidTest/java/in/aifi/mocr/ExampleInstrumentedTest.java
+++ mocr/src/androidTest/java/in/aifi/mocr/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("in.aifi.visualpipe", appContext.getPackageName());
+        assertEquals("in.aifi.mocr", appContext.getPackageName());
     }
 }
